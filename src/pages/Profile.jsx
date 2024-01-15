10dbc62518385e67e0176151a93a7e39437de6d7
Maribel Alarcon <maribelalarcon@macbook-air-4.lan>
2024-01-15 13:39:29 +0100

    comienzo de pagina profile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,4 +1,35 @@
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import Stack from "@mui/material/Stack";
+import Typography from "@mui/material/Typography";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import { useState } from "react";
+
 const Profile = () => {
-  return <h1>Profile</h1>;
+  const [firstName, setFirstName] = useState("");
+
+  return (
+    <Stack
+      component="form"
+      noValidate
+      autoComplete="off"
+      sx={{ width: 200 }}
+      spacing={2}
+    >
+      <TextField
+        id="filled-basic"
+        label="First Name"
+        value={firstName}
+        onChange={(e) => {
+          setFirstName(e.target.value);
+        }}
+      />
+      <TextField id="filled-basic" label="Last name" />
+      <Stack direction="horizontal">
+        <Typography variant="body1">Fecha de Nacimiento</Typography>
+        <DatePicker />
+      </Stack>
+    </Stack>
+  );
 };
 export default Profile;
