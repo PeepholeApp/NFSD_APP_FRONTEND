33eaf5650470e18d6fcfeb2d38ca58198bf694c4
Maribel <maribelsoledadalarcon@gmail.com>
2024-01-23 00:13:29 +0100

    delete import icon

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,7 +2,6 @@ import * as React from "react";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import IconButton from "@mui/material/IconButton";
-import MenuIcon from "@mui/icons-material/Menu";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
