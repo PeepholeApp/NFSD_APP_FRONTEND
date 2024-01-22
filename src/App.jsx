c1894649e032cf774b720bbb75603eff55830fb0
Maribel <maribelsoledadalarcon@gmail.com>
2024-01-23 00:43:41 +0100

    add themeProvider in App y I made changes en navBar

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,31 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Profile from "./pages/Profile";
 import Register from "./pages/Register";
-
+import { createTheme, ThemeProvider, styled } from "@mui/material/styles";
 import "./App.css";
+
+const theme = createTheme({
+  palette: {
+    mode: "light",
+    primary: {
+      main: "#654ea3",
+    },
+  },
+});
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/profile" element={<Profile />} />
-        </Routes>
-      </BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/profile" element={<Profile />} />
+          </Routes>
+        </BrowserRouter>
+      </ThemeProvider>
     </>
   );
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,30 +1,28 @@
 import * as React from "react";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
-import IconButton from "@mui/material/IconButton";
 import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import Home from "../pages/Home";
+import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
+  const navigate = useNavigate();
+
+  const onHome = () => {
+    navigate("/");
+  };
   return (
     <AppBar component="nav">
       <Toolbar>
-        <IconButton
-          color="#inherit"
-          aria-label="open drawer"
-          edge="start"
-          //   onClick={handleDrawerToggle}
-          sx={{ mr: 2, display: { sm: "none" } }}
-        ></IconButton>
-        <Typography
+        <Button
           variant="h6"
           component="div"
+          onClick={onHome}
           sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
         >
           FYP
-        </Typography>
+        </Button>
         <Box sx={{ display: { xs: "none", sm: "block" } }}>
           <Button key={"Home"} sx={{ color: "#fff" }}>
             Nosotros
