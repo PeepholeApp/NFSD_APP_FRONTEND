e8d4981d0c0383c77f32b64f454fa74c194612ed
Maribel <maribelsoledadalarcon@gmail.com>
2024-01-24 16:59:07 +0100

    NavBar added to profile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,6 +1,5 @@
 import TextField from "@mui/material/TextField";
 import Stack from "@mui/material/Stack";
-import Typography from "@mui/material/Typography";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 import { useState } from "react";
 import Radio from "@mui/material/Radio";
@@ -11,14 +10,13 @@ import FormLabel from "@mui/material/FormLabel";
 import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
 import Select from "@mui/material/Select";
-import Box from "@mui/material/Box";
 import Autocomplete from "@mui/material/Autocomplete";
 import Button from "@mui/material/Button";
 import axios from "axios";
-
 import Stepper from "@mui/material/Stepper";
 import Step from "@mui/material/Step";
 import StepLabel from "@mui/material/StepLabel";
+import NavBar from "../components/NavBar";
 
 const languagesOptions = [
   {
@@ -72,143 +70,146 @@ const Profile = () => {
   };
 
   return (
-    <Stack sx={{ m: 8 }} alignItems="center">
-      <Stack sx={{ width: 500 }} spacing={2}>
-        <Stepper activeStep={step}>
-          <Step>
-            <StepLabel>Informacion Personal</StepLabel>
-          </Step>
-          <Step>
-            <StepLabel>Intereses</StepLabel>
-          </Step>
-          <Step>
-            <StepLabel>Fotos</StepLabel>
-          </Step>
-        </Stepper>
+    <>
+      <NavBar />
+      <Stack sx={{ m: 15 }} alignItems="center">
+        <Stack sx={{ width: 500 }} spacing={2}>
+          <Stepper activeStep={step}>
+            <Step>
+              <StepLabel>Informacion Personal</StepLabel>
+            </Step>
+            <Step>
+              <StepLabel>Intereses</StepLabel>
+            </Step>
+            <Step>
+              <StepLabel>Fotos</StepLabel>
+            </Step>
+          </Stepper>
 
-        {step === 0 ? (
-          <>
-            <FormControl>
-              <TextField
-                label="First Name"
-                value={firstName}
-                onChange={(e) => {
-                  setFirstName(e.target.value);
-                }}
-              />
-            </FormControl>
+          {step === 0 ? (
+            <>
+              <FormControl>
+                <TextField
+                  label="First Name"
+                  value={firstName}
+                  onChange={(e) => {
+                    setFirstName(e.target.value);
+                  }}
+                />
+              </FormControl>
 
-            <FormControl>
-              <TextField
-                label="Last name"
-                value={lastName}
-                onChange={(e) => {
-                  setLastName(e.target.value);
-                }}
-              />
-            </FormControl>
+              <FormControl>
+                <TextField
+                  label="Last name"
+                  value={lastName}
+                  onChange={(e) => {
+                    setLastName(e.target.value);
+                  }}
+                />
+              </FormControl>
 
-            <Stack direction="row">
-              {/* <Typography variant="body1">Fecha de Nacimiento</Typography> */}
-              <FormLabel sx={{ paddingRight: 2 }}>
-                Fecha de Nacimiento
-              </FormLabel>
-              <DatePicker
-                value={brithday}
-                onChange={(date) => {
-                  setBrithday(date);
-                }}
-              />
-            </Stack>
+              <Stack direction="row">
+                {/* <Typography variant="body1">Fecha de Nacimiento</Typography> */}
+                <FormLabel sx={{ paddingRight: 2 }}>
+                  Fecha de Nacimiento
+                </FormLabel>
+                <DatePicker
+                  value={brithday}
+                  onChange={(date) => {
+                    setBrithday(date);
+                  }}
+                />
+              </Stack>
 
-            <FormControl>
-              <FormLabel>Genero</FormLabel>
-              <RadioGroup
-                row
-                value={genero}
-                onChange={(e) => {
-                  setGenero(e.target.value);
+              <FormControl>
+                <FormLabel>Genero</FormLabel>
+                <RadioGroup
+                  row
+                  value={genero}
+                  onChange={(e) => {
+                    setGenero(e.target.value);
+                  }}
+                >
+                  <FormControlLabel
+                    value="female"
+                    control={<Radio />}
+                    label="Femenino"
+                  />
+                  <FormControlLabel
+                    value="male"
+                    control={<Radio />}
+                    label="Masculino"
+                  />
+                  <FormControlLabel
+                    value="other"
+                    control={<Radio />}
+                    label="Otro"
+                  />
+                </RadioGroup>
+              </FormControl>
+
+              <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+                <InputLabel>Nacionalidad</InputLabel>
+                <Select
+                  value={nationality}
+                  label="nationality"
+                  onChange={(e) => {
+                    setNationality(e.target.value);
+                  }}
+                >
+                  <MenuItem value=""></MenuItem>
+                  <MenuItem value={10}>Argentina</MenuItem>
+                  <MenuItem value={20}>Colombia</MenuItem>
+                  <MenuItem value={20}>España</MenuItem>
+                  <MenuItem value={30}>Venezuela</MenuItem>
+                </Select>
+              </FormControl>
+
+              <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+                <Autocomplete
+                  value={languages}
+                  onChange={(e, value) => {
+                    setLanguages(value);
+                  }}
+                  multiple
+                  options={languagesOptions}
+                  // filterSelectedOptions
+                  renderInput={(params) => (
+                    <TextField {...params} label="Idioma" />
+                  )}
+                />
+              </FormControl>
+              <Stack direction="row">
+                <Button disabled={step === 0} onClick={onBack}>
+                  Back
+                </Button>
+                <Button onClick={onNext}>Avanzar a intereses personales</Button>
+              </Stack>
+            </>
+          ) : step === 1 ? (
+            <>
+              <FormControl
+                sx={{
+                  width: 500,
+                  maxWidth: "100%",
                 }}
               >
-                <FormControlLabel
-                  value="female"
-                  control={<Radio />}
-                  label="Femenino"
-                />
-                <FormControlLabel
-                  value="male"
-                  control={<Radio />}
-                  label="Masculino"
-                />
-                <FormControlLabel
-                  value="other"
-                  control={<Radio />}
-                  label="Otro"
-                />
-              </RadioGroup>
-            </FormControl>
-
-            <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
-              <InputLabel>Nacionalidad</InputLabel>
-              <Select
-                value={nationality}
-                label="nationality"
-                onChange={(e) => {
-                  setNationality(e.target.value);
-                }}
-              >
-                <MenuItem value=""></MenuItem>
-                <MenuItem value={10}>Argentina</MenuItem>
-                <MenuItem value={20}>Colombia</MenuItem>
-                <MenuItem value={20}>España</MenuItem>
-                <MenuItem value={30}>Venezuela</MenuItem>
-              </Select>
-            </FormControl>
-
-            <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
-              <Autocomplete
-                value={languages}
-                onChange={(e, value) => {
-                  setLanguages(value);
-                }}
-                multiple
-                options={languagesOptions}
-                // filterSelectedOptions
-                renderInput={(params) => (
-                  <TextField {...params} label="Idioma" />
-                )}
-              />
-            </FormControl>
-            <Stack direction="row">
-              <Button disabled={step === 0} onClick={onBack}>
-                Back
-              </Button>
-              <Button onClick={onNext}>Avanzar a intereses personales</Button>
-            </Stack>
-          </>
-        ) : step === 1 ? (
-          <>
-            <FormControl
-              sx={{
-                width: 500,
-                maxWidth: "100%",
-              }}
-            >
-              <TextField fullWidth label="Añade una Biografía sobre ti" />
-            </FormControl>
-            <Stack direction="row">
-              <Button onClick={onBack}>Back</Button>
-              <Button onClick={onNext}>Ahora sube tus fotos</Button>
-            </Stack>
-          </>
-        ) : step === 2 ? (
-          <>
-            <h1>Subir fotos</h1>
-            <Button onClick={onSave}>Guardar</Button>
-          </>
-        ) : null}
+                <TextField fullWidth label="Añade una Biografía sobre ti" />
+              </FormControl>
+              <Stack direction="row">
+                <Button onClick={onBack}>Back</Button>
+                <Button onClick={onNext}>Ahora sube tus fotos</Button>
+              </Stack>
+            </>
+          ) : step === 2 ? (
+            <>
+              <h1>Subir fotos</h1>
+              <Button onClick={onSave}>Guardar</Button>
+            </>
+          ) : null}
+        </Stack>
       </Stack>
-    </Stack>
+    </>
   );
 };
 export default Profile;
