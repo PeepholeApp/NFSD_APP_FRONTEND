0e9f21f3aa21a45cdcdfbfac25c06b81e9af6a4c
knoyag <146126313+knoyag@users.noreply.github.com>
2024-01-29 08:43:33 +0100

    token is saved in local storage using context

--- src/context/Login.jsx
+++ src/context/Login.jsx
@@ -1,16 +1,27 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useState, useEffect } from "react";
 
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
 
+  useEffect(() => {
+    // revisa en localstorage si el token existe
+    const storedToken = localStorage.getItem("token");
+    if (storedToken) {
+      setUser({ token: storedToken });
+    }
+  }, []);
+
   const login = (user) => {
     setUser(user);
+    // guarda el token en el local storage cuando se loggean 
+    localStorage.setItem("token", user.token);
   };
 
   const logout = () => {
     setUser(null);
+    localStorage.removeItem("token");
   };
 
   return (
