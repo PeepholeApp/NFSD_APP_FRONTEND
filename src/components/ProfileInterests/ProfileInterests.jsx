04e2fb2015d8578081474e9ff904c887a8815cca
Diego Martinez <diegomartinez@MacBook-Air-de-Diego.local>
2024-01-31 01:16:37 +0100

    Show icons in profile interests

--- src/components/ProfileInterests/ProfileInterests.jsx
+++ src/components/ProfileInterests/ProfileInterests.jsx
@@ -1,15 +1,26 @@
 import React from "react";
+import iconData from "../../data/interestIcons.json";
 import "./ProfileInterests.css";
 
-const interests = ["uno", "dos", "tres", "cuatro"];
-
 const ProfileInterests = ({ interests }) => {
+  const icons = iconData.interests;
+
+  const getIcon = (interestName) => {
+    const foundInterest = icons.find(
+      (interest) => interest.name === interestName
+    );
+    return foundInterest ? foundInterest.icon : null; // Retorna null si no se encuentra el icono
+  };
+
   return (
     <>
       <div>
         <div className="flex_interests">
           {interests.map((interest, index) => (
             <div key={index} className="style_interests">
+              <span role="img" aria-label="interest-icon">
+                {getIcon(interest[0])}
+              </span>
               {interest}
             </div>
           ))}
