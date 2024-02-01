161db1cc73d961c76096f93740705fb8e8c45845
Maribel <maribelsoledadalarcon@gmail.com>
2024-02-01 13:55:15 +0100

    creating component

---
+++ src/components/Button.jsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import Button from "@mui/material/Button";
+
+export default function Button() {
+  return (
+    <Button variant="contained" disableElevation>
+      Sign Up
+    </Button>
+  );
+}
