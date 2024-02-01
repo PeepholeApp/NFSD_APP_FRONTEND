e3a7bced775aa8cd27ac43e76146891ef851dd43
Maribel <maribelsoledadalarcon@gmail.com>
2024-02-01 20:44:19 +0100

    add button dark and light

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,36 @@
 import * as React from "react";
-import Button from "@mui/material/Button";
+import MaterialButton from "@mui/material/Button";
 
-export default function Button() {
+export function ButtonDark({ children, ...rest }) {
   return (
-    <Button variant="contained" disableElevation>
-      Sign Up
-    </Button>
+    <MaterialButton
+      variant="outlined"
+      disableElevation
+      sx={{
+        color: "#FFFFFF",
+        borderRadius: 10,
+        background: "#000000",
+      }}
+      {...rest}
+    >
+      {children}
+    </MaterialButton>
+  );
+}
+
+export function ButtonLight({ children, ...rest }) {
+  return (
+    <MaterialButton
+      variant="outlined"
+      disableElevation
+      sx={{
+        borderRadius: 10,
+        color: "#FFFFFF",
+        background: "linear-gradient(to right bottom, #3402B4, #6A1498)",
+      }}
+      {...rest}
+    >
+      {children}
+    </MaterialButton>
   );
 }
