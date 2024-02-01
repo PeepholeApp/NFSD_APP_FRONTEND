b1d88206ac29531da404ba71ea404ae934ea352f
Maribel <maribelsoledadalarcon@gmail.com>
2024-02-01 01:15:34 +0100

    added logo and modify styles

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -8,6 +8,10 @@ import { useNavigate } from "react-router-dom";
 import { useAuth } from "../context/Login";
 import IconButton from "@mui/material/IconButton";
 import Avatar from "@mui/material/Avatar";
+import logo from "../assets/logo.png";
+import Stack from "@mui/material/Stack";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
 
 const Navbar = () => {
   const { user } = useAuth();
@@ -20,26 +24,31 @@ const Navbar = () => {
   return user ? (
     <>
       <AppBar component="nav" position="static">
-        <Toolbar>
-          <Button
-            variant="h6"
-            component="div"
-            onClick={onHome}
-            sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
-          >
-            FYP
-          </Button>
-          <Box sx={{ display: { xs: "none", sm: "block" } }}>
+        <Toolbar
+          sx={{
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          <Box onClick={onHome}>
+            <img src={logo} alt="logo" style={{ width: 100 }} />
+          </Box>
+
+          <Box>
             <Button key={"Home"} sx={{ color: "#fff" }}>
-              App
-            </Button>
-            <Button key={"Home"} sx={{ color: "#fff" }}>
-              About us
+              Comunity
             </Button>
             <Button key={"Home"} sx={{ color: "#fff" }}>
               Blog
             </Button>
+            <Button key={"Home"} sx={{ color: "#fff" }}>
+              About us
+            </Button>
+            Contact
           </Box>
+
+          <Avatar></Avatar>
         </Toolbar>
       </AppBar>
     </>
