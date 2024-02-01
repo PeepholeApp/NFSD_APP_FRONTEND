66b6c68311539a8f69e3d1f5aa9950f605244d2b
Maribel <maribelsoledadalarcon@gmail.com>
2024-02-01 20:42:15 +0100

    add test button

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,17 @@
 import { Link } from "react-router-dom";
+import { ButtonDark, ButtonLight } from "../components/Button";
+
+function onHi() {
+  console.log("Hola");
+}
 
 const Home = () => {
   return (
     <>
       <Link to="/profile">Profile</Link>
       <h1>Home</h1>
+      <ButtonDark onClick={onHi}>Sign Up</ButtonDark>
+      <ButtonLight>Sign In</ButtonLight>
     </>
   );
 };
