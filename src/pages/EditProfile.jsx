b7e47754297ad7f20e1dd164271b7169c0e50b98
Maribel <maribelsoledadalarcon@gmail.com>
2024-02-07 11:59:38 +0100

    added fields

--- src/pages/EditProfile.jsx
+++ src/pages/EditProfile.jsx
@@ -9,19 +9,48 @@ import { deepPurple } from "@mui/material/colors";
 import Icon from "@mui/material/Icon";
 import { useTheme } from "@mui/material/styles";
 import { useAuth } from "../context/Login";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Radio from "@mui/material/Radio";
+import RadioGroup from "@mui/material/RadioGroup";
+import InputLabel from "@mui/material/InputLabel";
+import Select from "@mui/material/Select";
+import MenuItem from "@mui/material/MenuItem";
+import Autocomplete from "@mui/material/Autocomplete";
 
-const useIsDarkMode = () => {
-  const theme = useTheme();
-  return theme.palette.mode === "dark";
-};
+const languagesOptions = [
+  {
+    label: "Español",
+    value: "ES",
+  },
+  {
+    label: "Ingles",
+    value: "EN",
+  },
+  {
+    label: "Frances",
+    value: "FR",
+  },
+  {
+    label: "Portugues",
+    value: "PR",
+  },
+  {
+    label: "Italiano",
+    value: "IT",
+  },
+];
 
 const EditProfile = () => {
-  const [brithday, setBrithday] = useState(new Date());
   const [name, setName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [birthday, setBirthday] = useState(new Date());
+  const [nationality, setNationality] = useState("");
+  const [genero, setGenero] = useState("");
+  const [languages, setLanguages] = useState([]);
+  const [bio, setBio] = useState("");
+  const [countries, setCountries] = useState([]);
 
   const { user, loading } = useAuth();
-
-  const isDarkMode = useIsDarkMode();
 
   useEffect(() => {
     if (user && !loading) {
@@ -32,6 +61,12 @@ const EditProfile = () => {
           );
           const profile = response.data;
           setName(profile.name);
+          setLastName(profile.last_name);
+          setBirthday(new Date(profile.dob));
+          setBio(profile.bio);
+          setGenero(profile.gender);
+          setNationality(profile.nationality);
+          setLanguages(profile.languages);
         } catch (error) {
           console.error(error);
         }
@@ -40,9 +75,21 @@ const EditProfile = () => {
     }
   }, [loading, user]);
 
-  const onNameChange = (event) => {
-    setName(event.target.value);
-  };
+  useEffect(() => {
+    axios.get("https://restcountries.com/v3.1/all").then((response) => {
+      const countries = response.data
+        .map((country) => {
+          return {
+            label: country.name.common,
+            value: country.cca2,
+          };
+        })
+        .sort((countryA, countryB) => {
+          return countryA.label.localeCompare(countryB.label);
+        });
+      setCountries(countries);
+    });
+  }, []);
 
   return (
     <Stack sx={{ m: 5 }} alignItems="center">
@@ -58,27 +105,45 @@ const EditProfile = () => {
           P
         </Avatar>
         <Stack direction="row" spacing={2}>
-          <TextField value={name} onChange={onNameChange} />
-
-          <TextField label="Last name" />
+          <TextField
+            value={name}
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+          />
+          <TextField
+            value={lastName}
+            onChange={(e) => {
+              setLastName(e.target.value);
+            }}
+          />
         </Stack>
         <Stack direction="row">
           <FormLabel sx={{ paddingRight: 2 }}>Fecha de Nacimiento</FormLabel>
-          <DatePicker value={brithday} />
+          <DatePicker
+            value={birthday}
+            onChange={(e) => {
+              setBrithday(e.target.value);
+            }}
+          />
         </Stack>
-        <TextField fullWidth label="Añade una Biografía sobre ti" />
-        <Stack>
-          <Icon
-            sx={{ ...(isDarkMode && { filter: "invert(1)" }) }}
-            baseClassName="material-icons-two-tone"
-          >
-            +
-          </Icon>
-        </Stack>
+        <TextField
+          value={bio}
+          onChange={(e) => {
+            setBio(e.target.value);
+          }}
+          fullWidth
+        />
 
-        {/* <FormControl>
+        <FormControl>
           <FormLabel>Genero</FormLabel>
-          <RadioGroup row value={genero}>
+          <RadioGroup
+            row
+            value={genero}
+            onChange={(e) => {
+              setGenero(e.target.value);
+            }}
+          >
             <FormControlLabel
               value="female"
               control={<Radio />}
@@ -91,7 +156,35 @@ const EditProfile = () => {
             />
             <FormControlLabel value="other" control={<Radio />} label="Otro" />
           </RadioGroup>
-        </FormControl> */}
+        </FormControl>
+        <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+          <InputLabel>Nacionalidad</InputLabel>
+          <Select
+            value={nationality}
+            label="nationality"
+            onChange={(e) => {
+              setNationality(e.target.value);
+            }}
+          >
+            {countries.map((option) => (
+              <MenuItem key={option.value} value={option.value}>
+                {option.label}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+
+        <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+          <Autocomplete
+            value={languages}
+            onChange={(e, value) => {
+              setLanguages(value);
+            }}
+            multiple
+            options={languagesOptions}
+            renderInput={(params) => <TextField {...params} label="Idioma" />}
+          />
+        </FormControl>
       </Stack>
     </Stack>
   );
