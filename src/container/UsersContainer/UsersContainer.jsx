575e0e206485c838edcdbe0ca09dfe2501b77a89
Diego Martinez <diegomartinez@MacBook-Air-de-Diego.local>
2024-02-07 01:24:46 +0100

    add icon gender in info userContainer

--- src/container/UsersContainer/UsersContainer.jsx
+++ src/container/UsersContainer/UsersContainer.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import ProfileInterests from "../../components/ProfileInterests/ProfileInterests";
 import ProfileLanguages from "../../components/ProfileLanguages/ProfileLanguages";
+import gendersDataIcons from "../../data/genders.json";
 import "./UsersContainer.css";
 
 const UsersContainer = ({ profiles }) => {
@@ -11,6 +12,26 @@ const UsersContainer = ({ profiles }) => {
     const fechaNacimientoMoment = moment(age);
     const edad = moment().diff(fechaNacimientoMoment, "years");
     return edad;
+  };
+
+  const getIconImg = (type, data) => {
+    let foundIcon = {};
+
+    if (type === "gender") {
+      foundIcon = gendersDataIcons.genders.find(
+        (gender) => gender.name === data
+      );
+    }
+    if (type === "language") {
+      foundIcon = languagesDataIcons.languages.find(
+        (language) => language.shortName === data
+      );
+    }
+
+    if (foundIcon) {
+      return foundIcon.icon;
+    }
+    return null;
   };
 
   return (
@@ -41,7 +62,7 @@ const UsersContainer = ({ profiles }) => {
               <div className="divisionData"></div>
               <div className="flexData">
                 <div className="titleUserData">Gender</div>
-                <div className="subtitleUserData">{profile.gender}</div>
+                <span role="img">{getIconImg("gender", profile.gender)}</span>
               </div>
             </div>
             <div className="card_details">
