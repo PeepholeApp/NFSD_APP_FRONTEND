85cab10834113cdfe8eb71acf6e512def6525440
Maribel <maribelsoledadalarcon@gmail.com>
2024-02-23 16:15:22 +0100

    added AuthProvider en main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,12 @@ import App from "./App.jsx";
 import "./index.css";
 import { LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
+import { AuthProvider } from "./context/Login";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <LocalizationProvider dateAdapter={AdapterDateFns}>
-    <App />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </LocalizationProvider>
 );
