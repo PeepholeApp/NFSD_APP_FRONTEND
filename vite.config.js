7ae93d75cceb5b524c89dad43b73eb900a07d091
Diego Martinez <diegomartinez@MacBook-Air-de-Diego.local>
2024-02-27 22:05:10 +0100

    add config

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
+import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // Other Vite config options...
-  esbuild: {
-    target: "esnext",
-    platform: "linux",
-  },
-  // Other Vite config options...
+  plugins: [react()],
 });
