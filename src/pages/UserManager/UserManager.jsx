856970e5e17ab7f018b45a66268aac50a7b1f5f6
Diego Martinez <diegomrey1@gmail.com>
2024-03-11 01:33:25 +0100

    update user with select option

--- src/pages/UserManager/UserManager.jsx
+++ src/pages/UserManager/UserManager.jsx
@@ -3,16 +3,41 @@ import React, { useState } from "react";
 import "./UserManager.css";
 
 const UserManager = () => {
-  const [user, setUser] = useState({});
+  const [user, setUser] = useState();
   const [userEmail, setUserEmail] = useState("");
+  const [editToggle, setEditToggle] = useState(false);
+  const [role, setRole] = useState("");
 
   const getUserByEmail = async () => {
     try {
       const response = await axios.get(
         `${import.meta.env.VITE_API_URL}/profiles/search/${userEmail}`
       );
-      console.log("user:", response.data);
       setUser(response.data);
+    } catch (error) {
+      console.log(error);
+    } finally {
+    }
+  };
+
+  const deleteUser = async () => {
+    try {
+      const response = await axios.get(
+        `${import.meta.env.VITE_API_URL}/profiles/search/${userEmail}`
+      );
+      setUser(response.data);
+    } catch (error) {
+      console.log(error);
+    } finally {
+    }
+  };
+
+  const updateUser = async (userId) => {
+    try {
+      await axios.patch(`${import.meta.env.VITE_API_URL}/users/${userId}`, {
+        role,
+      });
+      setEditToggle(true);
     } catch (error) {
       console.log(error);
     } finally {
@@ -30,10 +55,25 @@ const UserManager = () => {
         </div>
         <div className="background1 getUserContainer">
           <h2>User details</h2>
-          {/* <h3>{user && user.profile.name}</h3>
-          <h3>{user && user.profile.last_name}</h3>
-          <h3>{user && user.user.role}</h3> */}
-          <button onClick={() => console.log(user)}>Edit</button>
+          <h3>
+            {user && user.profile.name} {user && user.profile.last_name}
+          </h3>
+          {editToggle ? (
+            <select name="" id="" onChange={(e) => setRole(e.target.value)}>
+              <option value="admin">admin</option>
+              <option value="user">user</option>
+            </select>
+          ) : (
+            <h3>{user && user.user.role}</h3>
+          )}
+          <button onClick={() => setEditToggle(!editToggle)}>
+            {editToggle ? "Cancel" : "Edit"}
+          </button>
+          {editToggle ? (
+            <button onClick={() => updateUser(user.user._id)}>Accept</button>
+          ) : (
+            <></>
+          )}
         </div>
       </div>
     </>
