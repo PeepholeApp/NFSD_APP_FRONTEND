eac095c09b5bd7565fd7cb665c2c8044cb33562a
Diego Martinez <diegomrey1@gmail.com>
2024-03-11 18:19:21 +0100

    fix fix fixxxx fiiiiiiixx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,15 @@
 import CssBaseline from "@mui/material/CssBaseline";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import React, { useEffect, useState } from "react";
-import {
-  BrowserRouter,
-  Navigate,
-  Route,
-  Routes,
-  useParams,
-} from "react-router-dom";
+import { Navigate, Route, Routes, useParams } from "react-router-dom";
 import initializeNotifications from "../notifications";
 import Navbar from "./components/NavBar";
-import Dashboard from "./container/Dashboard/Dashboard";
+import { ChatContextProvider } from "./context/ChatContext";
 import { FiltersProvider } from "./context/FiltersContext";
 import { useAuth } from "./context/Login";
 import AboutUs from "./pages/AboutUs";
 import Blog from "./pages/Blog/Blog";
+import Chat from "./pages/Chat/Chat";
 import Community from "./pages/Community/Community";
 import Contact from "./pages/Contact";
 import EditProfile from "./pages/EditProfile";
@@ -23,14 +18,9 @@ import Home from "./pages/Home";
 import HomeApp from "./pages/HomeApp/HomeApp";
 import Login from "./pages/Login";
 import Profile from "./pages/Profile";
-import RecoverPassword from "./pages/RecoverPassword";
 import Register from "./pages/Register";
 import UserDetails from "./pages/UserDetails/UserDetails";
-import Chat from "./pages/Chat/Chat";
-import io from "socket.io-client";
-import { ChatContextProvider } from './context/ChatContext';
 import UserManager from "./pages/UserManager/UserManager";
-
 
 const theme = createTheme({
   palette: {
@@ -69,7 +59,6 @@ function App() {
   const { user, loading } = useAuth();
   const [newNotifications, setNewNotifications] = useState(false);
 
-
   useEffect(() => {
     if (user) {
       initializeNotifications(user, () => setNewNotifications(true));
@@ -79,42 +68,44 @@ function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
-          <FiltersProvider>
-            <CssBaseline />
-            <ChatContextProvider user={user}>
+        <FiltersProvider>
+          <CssBaseline />
+          <ChatContextProvider user={user}>
             <Navbar />
-              <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/register" element={<Register />} />
-                <Route path="/forgot-password" element={<ForgotPassword />} />
-                <Route
-                  path="/profile"
-                  element={<PrivateRoute element={<Profile />} />}
-                />
-                <Route
-                  path="/editProfile"
-                  element={<PrivateRoute element={<EditProfile />} />}
-                />
-                <Route
-                  path="/home"
-                  element={<PrivateRoute element={<HomeApp />} />}
-                />
-                <Route path="/user/:userId" element={<UserDetailsWrapper />} />
-                <Route
-                  path="/community"
-                  element={<PrivateRoute element={<Community />} />}
-                />
-                <Route path="/blog" element={<Blog />} />
-                <Route path="/aboutUs" element={<AboutUs />} />
-                <Route path="/contact" element={<Contact />} />
-                <Route path="/chat" 
-                  element={<PrivateRoute element={<Chat />} />} 
-                  />
-                <Route path="*" element={<Navigate to="/" />} />
-              </Routes>
-            </ChatContextProvider>
-          </FiltersProvider>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/forgot-password" element={<ForgotPassword />} />
+              <Route
+                path="/profile"
+                element={<PrivateRoute element={<Profile />} />}
+              />
+              <Route
+                path="/editProfile"
+                element={<PrivateRoute element={<EditProfile />} />}
+              />
+              <Route
+                path="/home"
+                element={<PrivateRoute element={<HomeApp />} />}
+              />
+              <Route path="/user/:userId" element={<UserDetailsWrapper />} />
+              <Route
+                path="/community"
+                element={<PrivateRoute element={<Community />} />}
+              />
+              <Route path="/blog" element={<Blog />} />
+              <Route path="/aboutUs" element={<AboutUs />} />
+              <Route path="/contact" element={<Contact />} />
+              <Route
+                path="/chat"
+                element={<PrivateRoute element={<Chat />} />}
+              />
+              <Route path="/userManager" element={<UserManager />} />
+              <Route path="*" element={<Navigate to="/" />} />
+            </Routes>
+          </ChatContextProvider>
+        </FiltersProvider>
       </ThemeProvider>
     </>
   );
